231496a03bf353ecdf0ab4fb925b92d15f173998
YeisonArleyBejaranoMartinez <yeisonb22@hotamil.com>
2023-02-28 23:33:09 -0500

    (feat): se crea archivo de main, layout, nuevoCliente

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import{createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+import NuevoCliente from './pages/NuevoCliente'
+import Index, {loader as clientesLoader} from './pages/Index'
+
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <h1>inicio</h1>
-  },
-{
-  path: '/nosotros',
-  element: <h1>Nosotros</h1>
-
-}
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Index/>,
+        loader: clientesLoader 
+      },
+      {
+      path: '/clientes/nuevo',
+      element: <NuevoCliente />
+    }]
+  }
 ])
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
