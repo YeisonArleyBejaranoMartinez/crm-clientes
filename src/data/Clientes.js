b18b017642dac777a3da493a1433ce60673f4225
YeisonArleyBejaranoMartinez <yeisonb22@hotamil.com>
2023-04-26 21:45:38 -0500

    se  termina ´ pryecto

--- src/data/Clientes.js
+++ src/data/Clientes.js
@@ -1,27 +1,52 @@
-import { json } from "react-router";
-
-export async function obtenerClientes(){
+export async function obtenerClientes() {
     const respuesta = await fetch(import.meta.env.VITE_API_URL)
-    const resultado = await respuesta.json();
+    const resultado = await respuesta.json()
     return resultado
 }
 
 export async function obtenerCliente(id) {
-  const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`);
-  const resultado = await respuesta.json();
-  return resultado;
+    const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`)
+    const resultado = await respuesta.json()
+    return resultado
 }
-export async function agregarClientes(datos){
-  try {
-    const respuesta = await fetch(import.meta.env.VITE_API_URL, {
-      method: "POST",
-      body: JSON.stringify(datos),
-      headers:{
-        'Content-Type':'application/json'
-      }
-    });
-    
-  } catch (error) {
-    console.log(error)
-  }
+
+export async function agregarCliente(datos) {
+    try {
+        const respuesta = await fetch(import.meta.env.VITE_API_URL, {
+            method: 'POST',
+            body: JSON.stringify(datos),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        await respuesta.json()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function actualizarCliente(id, datos) {
+    try {
+        const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(datos),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        await respuesta.json()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function eliminarCliente(id) {
+        try {
+        const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`, {
+            method: 'DELETE'
+        })
+        await respuesta.json()
+    } catch (error) {
+        console.log(error)
+    }
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout'
-import NuevoCliente, {action as nuevoClienteAction} from './pages/NuevoCliente'
-import Index, {loader as clientesLoader} from './pages/Index'
+import NuevoCliente, {action as nuevoClienteAction } from './pages/NuevoCliente'
+import Index, { loader as clientesLoader } from './pages/Index'
+import EditarCliente, { loader as editarClienteLoader, action as editarClienteAction } from './pages/EditarCliente'
 import ErrorPage from './components/ErrorPage'
-import EditarCliente, {loader as editarClienteLoader} from './pages/EditarCliente'
+import { action as eliminarClienteAction } from './components/Cliente'
+
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Layout />,
+    path: '/',
+    element: <Layout />, 
     children: [
       {
         index: true,
         element: <Index />,
         loader: clientesLoader,
-        errorElement: <ErrorPage/>
+        errorElement: <ErrorPage />
       },
       {
-        path: "/clientes/nuevo",
+        path: '/clientes/nuevo',
         element: <NuevoCliente />,
         action: nuevoClienteAction,
-        errorElement:<ErrorPage/>
+        errorElement: <ErrorPage />
       },
       {
         path: '/clientes/:clienteId/editar',
-        element:<EditarCliente/>,
+        element: <EditarCliente />,
         loader: editarClienteLoader,
-        errorElement: <ErrorPage/>
+        action: editarClienteAction,
+        errorElement: <ErrorPage />
+      }, 
+      {
+        path: '/clientes/:clienteId/eliminar',
+        action: eliminarClienteAction
       }
-    ],
+    ]
   },
-]);
-ReactDOM.createRoot(document.getElementById("root")).render(
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
